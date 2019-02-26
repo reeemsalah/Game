392101a034bca903ff005f2223b758d28ec1d4f5
reeemsalah <47950161+reeemsalah@users.noreply.github.com>
2019-02-26 17:53:27 +0200

    ReadFile method

---
+++ Test/src/simulation/Simulator.java
@@ -0,0 +1,28 @@
+package simulation;
+
+import java.util.ArrayList;
+
+import model.disaster.Disaster;
+import model.infrastructure.ResidentialBuilding;
+
+public class Simulator {
+	private int currentCycle;
+	private ArrayList<ResidentialBuilding> buildings;
+	private ArrayList<Citizen> citizens;
+	private ArrayList<Unit> emergencyUnits;
+	private ArrayList<Disaster> plannedDisasters;
+	private ArrayList<Disaster> executedDisaster;
+	private Address[][] world;
+	
+
+	public Simulator() {
+		// TODO Auto-generated constructor stub
+		world=new Address[10][10];
+		for (int i=0;i<world.length;i++)
+		{
+			for(int j=0;j<world[i].length;j++)
+				world[i][j]=new Address(i,j);
+		}
+	}
+
+}
--- Test/src/tests/Test.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class Test {
-
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
