9b5701ecc89973b01991de081f44a7ee517999f6
youssefshaarawy <youssefshaarawy@192.168.1.102>
2019-02-26 19:01:11 +0200

    hi

--- Test/src/model/units/Evacuator.java
+++
@@ -1,7 +0,0 @@
-package model.units;
-
-public class Evacuator extends PoliceUnit{
-	public Evacuator(String id, Address location, int stepsPerCycle, int maxCapacity) {
-		super(id, location, stepsPerCycle, maxCapacity);
-	}
-}
