489c76c7beeeb4482b55a5ed62853b9fb959096b
youssefshaarawy <youssefshaarawy@192.168.1.102>
2019-02-26 18:25:33 +0200

    Java > prolog

---
+++ Test/src/model/units/Evacuator.java
@@ -0,0 +1,7 @@
+package model.units;
+
+public class Evacuator extends PoliceUnit{
+	public Evacuator(String id, Address location, int stepsPerCycle, int maxCapacity) {
+		super(id, location, stepsPerCycle, maxCapacity);
+	}
+}
